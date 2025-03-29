6366c8a1803559bbf1c6dc26c8352d49be2c16aa
Gabriel Fernando <gf313074@gmail.com>
2025-03-28 21:56:33 -0300

    create button component and error tratament

---
+++ src/components/forms/button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './button.module.css';
+
+type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = ({ children, ...props }: ButtonType) => {
+  return (
+    <button {...props} className={style.button}>
+      {children}
+    </button>
+  );
+};
