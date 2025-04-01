3a8202313961e5bab4f5e157c69370fb0e2709ca
Gabriel Fernando <gf313074@gmail.com>
2025-04-01 11:19:36 -0300

    fix bugs

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,12 +1,20 @@
+import photosGet, { Photo } from '@/actions/photos-get';
+import Feed from '@/components/Feed/feed';
+
 export default async function PerfilUserPage({
   params,
 }: {
   params: Promise<{ user: string }>;
 }) {
-  const resolvedParam = await Promise.resolve(params);
+  const resolvedParams = await Promise.resolve(params);
+
+  const { data } = await photosGet({ user: resolvedParams.user });
+
+  if (!data) return null;
   return (
-    <main>
-      <h1>Usuário: {resolvedParam.user}</h1>
-    </main>
+    <section className='container mainSection'>
+      <h1 className='title'>{resolvedParams.user}</h1>
+      <Feed photos={data as Photo[]} user={resolvedParams.user} />
+    </section>
   );
 }
