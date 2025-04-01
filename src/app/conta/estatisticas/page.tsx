63ca75d2123a7cf4a17f35e73eb38b9640274fdc
Gabriel Fernando <gf313074@gmail.com>
2025-03-31 23:01:59 -0300

    add stats graphs by posts

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,3 +1,27 @@
+import statsGet, { StatsData } from '@/actions/stats-get';
+import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+
+const ContaEstatisticas = dynamic(
+  () => import('@/components/conta/conta-estatisticas'),
+  {
+    loading: () => <p>Carregando...</p>,
+  },
+);
+
+export const metadata: Metadata = {
+  title: 'Estatísticas | Minha Conta',
+};
+
 export default async function EstatisticasPage() {
-  
+  const { data } = await statsGet();
+  const resolvedData = (await data) as StatsData[];
+
+  if (!resolvedData) return null;
+
+  return (
+    <section>
+      <ContaEstatisticas data={resolvedData} />
+    </section>
+  );
 }
